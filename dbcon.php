e22642fe61fd65b3aa5f63ca75277d55e0d64d0a
laevad <aldave.tabian@ustp.edu.ph>
2022-10-31 11:39:24 +0800

    CHANGES

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","lms");
+$con = mysqli_connect("containers-us-west-109.railway.app","root","Ia58IF3p7mnv53QxLe6P","railway",5617);
 
 if(!$con){
     die('Connection Failed'. mysqli_connect_error());
